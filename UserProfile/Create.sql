d611dc76a17e51a0f8c4fbd039a32b962950b141
AyyubPatel <apatel55@luc.edu>
2016-04-28 11:36:01 -0500

    Create table statement added

---
+++ UserProfile/Create.sql
@@ -0,0 +1,11 @@
+Create table User_profile
+    
+    ( 
+	  userId varchar(20),
+      numberOfMatches int,
+      win int,
+      lose int,
+      rank int,
+      winProbability decimal(3,2),
+      status varchar(3)
+	)
